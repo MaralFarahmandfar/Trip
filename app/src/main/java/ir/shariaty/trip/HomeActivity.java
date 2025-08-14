a6c4b30e87584a90d3228ff4f49b9b146c1fa62b
pan-afs <p.afshar3001@gmail.com>
2025-08-14 14:44:32 +0330

    solve a problem with toasts and fix some visual things

--- app/src/main/java/ir/shariaty/trip/HomeActivity.java
+++ app/src/main/java/ir/shariaty/trip/HomeActivity.java
@@ -7,7 +7,6 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
--- app/src/main/java/ir/shariaty/trip/SignUpActivity.java
+++ app/src/main/java/ir/shariaty/trip/SignUpActivity.java
@@ -10,6 +10,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
